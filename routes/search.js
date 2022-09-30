a671b42fd0cd67dc21db16ebd777e7fbf1a962f9
Igal <igalbog@gmail.com>
2022-10-01 01:55:41 +0300

    added channel search

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const itemsQuery = "ol > li.video-listing-entry > article";
+
 router.get("/video", async (req, res) => {
   const {
     q,
@@ -36,7 +38,7 @@ router.get("/video", async (req, res) => {
     return;
   }
 
-  $("ol > li.video-listing-entry > article.video-item", data).each(function () {
+  $(itemsQuery).each(function () {
     results.push({
       title: $(this).find("h3.video-item--title").text(),
       url: `https://rumble.com${$(this)
@@ -47,10 +49,54 @@ router.get("/video", async (req, res) => {
       channel_url: `https://rumble.com${$(this)
         .find("a[rel='author']")
         .attr("href")}`,
+      verified: $(this).find(
+        "a[rel='author'] > div > svg.verification-badge-icon"
+      ).length
+        ? true
+        : false,
       duration: $(this).find(".video-item--duration").attr("data-value"),
       views: $(this).find(".video-item--views").attr("data-value"),
       rumbles: $(this).find(".video-item--rumbles").attr("data-value"),
       date: $(this).find(".video-item--time").attr("datetime"),
+    });
+  });
+
+  res.json(results);
+});
+
+router.get("/channel", async (req, res) => {
+  const { q } = req.query;
+  const results = [];
+  let data;
+  let $;
+
+  if (!q) {
+    res.status(400).json("Search term missing");
+    return;
+  }
+
+  try {
+    const response = await axios.get(
+      `https://rumble.com/search/channel?q=${q}`
+    );
+    data = response?.data;
+    $ = cheerio.load(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+    return;
+  }
+
+  $(itemsQuery).each(function () {
+    results.push({
+      url: `https://rumble.com${$(this)
+        .find("a[class^=channel-item]")
+        .attr("href")}`,
+      title: $(this).find("h3.channel-item--title").text(),
+      verified: $(this).find("a > h3 > svg.verification-badge-icon").length
+        ? true
+        : false,
+      subscribers: $(this).find(".channel-item--subscribers").text(),
     });
   });
 
