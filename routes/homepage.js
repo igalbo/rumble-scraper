96bb42dbab19b34b84cfa490d51d89292f87d242
Igal <igalbog@gmail.com>
2022-10-05 23:23:47 +0300

    fixed crash

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-router.get("/:video", async (req, res) => {
+router.get("/", async (req, res) => {
+  const editorPicks = [];
   const result = [];
-  const related = [];
-  const { video } = req.params;
   let $ = "";
   let data;
   try {
-    response = await axios.get(`https://rumble.com/${video}`);
+    const response = await axios.get("https://rumble.com/");
     data = response?.data;
     $ = cheerio.load(data);
   } catch (err) {
@@ -18,7 +17,62 @@ router.get("/:video", async (req, res) => {
     res.status(400).json(err);
     return;
   }
+  $("div.tab-editor-picks > ul.mediaList-list > li").each(function () {
+    editorPicks.push({
+      title: $(this).children().children().find("h3").attr("title"),
+      channel: $(this).children().children().find("h4").text(),
+      url: `https://rumble.com${$(this).children("a").attr("href")}`,
+      thumbnail: $(this).find(".mediaList-image").attr("src"),
+      duration: $(this).find(".mediaList-duration").text(),
+      watching: $(this).find(".mediaList-liveCount").text() || undefined,
+    });
+  });
+  const topVideosData = $("article:has(h1:contains('Top Videos'))");
+  const topVideos = [
+    {
+      title: topVideosData.find("h3").attr("title"),
+      channel: topVideosData.find("h4.mediaList-by-heading").text(),
+      url: `https://rumble.com${topVideosData
+        .find("a.mediaList-link")
+        .attr("href")}`,
+      thumbnail: topVideosData.find(".mediaList-image").attr("src"),
+      date: topVideosData.find(".mediaList-timestamp").text(),
+      watching: topVideosData.find(".mediaList-liveCount").text() || undefined,
+      views: topVideosData.find(".mediaList-plays").text(),
+    },
+  ];
+  $("section > a").each(function () {
+    const category = $(this)
+      .attr("href")
+      .replace("/category", "")
+      .replace("/", "");
+    result.push({
+      [category.replace("-", "_")]: fetchCategoryItems(data, category),
+    });
+  });
+  res.json([
+    { top_videos: topVideos },
+    { editor_picks: editorPicks },
+    ...result,
+  ]);
+});
 
+router.get("/:video", async (req, res) => {
+  const result = [];
+  const related = [];
+  const { video } = req.params;
+  let $ = "";
+  let data;
+
+  try {
+    const response = await axios.get(`https://rumble.com/${video}`);
+    data = response?.data;
+    $ = cheerio.load(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+    return;
+  }
   result.push({
     title: $("article > h1.h1").text(),
     channel_url: `https://rumble.com${$(".media-by--a").attr("href")}`,
@@ -37,13 +91,11 @@ router.get("/:video", async (req, res) => {
     views: $(".media-heading-info:not(.media-heading-published)")
       .text()
       .replace(" Views", ""),
-
     subscribers: $(".subscribe-button-count").text().substring(1),
     streamed: $(".streamed-on > time").attr("datetime"),
     rumbles: $("span.rumbles-count").text(),
   });
-
-  $("ul.mediaList-list > li", data).each(function () {
+  $("ul.mediaList-list > li").each(function () {
     related.push({
       title: $(this).children().children().find("h3").attr("title"),
       channel: $(this).children().children().find("h4").text(),
@@ -53,67 +105,7 @@ router.get("/:video", async (req, res) => {
       watching: $(this).find(".mediaList-liveCount").text() || undefined,
     });
   });
-
   res.json({ video: result, related });
-});
-
-router.get("/", async (req, res) => {
-  const editorPicks = [];
-  const result = [];
-  let $ = "";
-  let data;
-  try {
-    response = await axios.get("https://rumble.com/");
-    data = response?.data;
-    $ = cheerio.load(data);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-    return;
-  }
-
-  $("div.tab-editor-picks > ul.mediaList-list > li", data).each(function () {
-    editorPicks.push({
-      title: $(this).children().children().find("h3").attr("title"),
-      channel: $(this).children().children().find("h4").text(),
-      url: `https://rumble.com${$(this).children("a").attr("href")}`,
-      thumbnail: $(this).find(".mediaList-image").attr("src"),
-      duration: $(this).find(".mediaList-duration").text(),
-      watching: $(this).find(".mediaList-liveCount").text() || undefined,
-    });
-  });
-
-  const topVideosData = $("article:has(h1:contains('Top Videos'))");
-  const topVideos = [
-    {
-      title: topVideosData.find("h3").attr("title"),
-      channel: topVideosData.find("h4.mediaList-by-heading").text(),
-      url: `https://rumble.com${topVideosData
-        .find("a.mediaList-link")
-        .attr("href")}`,
-      thumbnail: topVideosData.find(".mediaList-image").attr("src"),
-      date: topVideosData.find(".mediaList-timestamp").text(),
-      watching: topVideosData.find(".mediaList-liveCount").text() || undefined,
-      views: topVideosData.find(".mediaList-plays").text(),
-    },
-  ];
-
-  $("section > a").each(function () {
-    const category = $(this)
-      .attr("href")
-      .replace("/category", "")
-      .replace("/", "");
-
-    result.push({
-      [category.replace("-", "_")]: fetchCategoryItems(data, category),
-    });
-  });
-
-  res.json([
-    { top_videos: topVideos },
-    { editor_picks: editorPicks },
-    ...result,
-  ]);
 });
 
 const fetchCategoryItems = (data, category) => {
